e7efb0d26e75aacdea235c01b8dedccdb02ed52b
Hendrik Brinkmann <brinkmann@sipgate.de>
2024-03-19 16:10:20 +0100

    test: add more tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,36 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
-    }
+  @Test
+  void foo() {
+    final Item[] items = new Item[] {new Item("foo", 0, 0)};
+    final GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals("foo", app.items[0].name);
+  }
 
+  @Test
+  void simpleItemShouldSpoilRegulary() {
+    final Item[] items = new Item[] {new Item("Elixir of the Mongoose", 5, 7)};
+    final GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals("Conjured Mana Cake", app.items[0].name);
+    assertEquals(4, app.items[0].sellIn);
+    assertEquals(6, app.items[0].quality);
+  }
+
+  @Test
+  void conjuredItemsShouldSpoilTwiceAsFast() {
+    final Item[] items = new Item[] {new Item("Conjured Mana Cake", 3, 6)};
+    final GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals("Conjured Mana Cake", app.items[0].name);
+    assertEquals(2, app.items[0].sellIn);
+    assertEquals(4, app.items[0].quality);
+  }
 }
